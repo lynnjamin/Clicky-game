d261022bf23e9133bfbbebdfaa24c2e4bb45e8a9
Lynn Tang <tanglynn2@gmail.com>
2019-04-30 19:40:12 -0500

    Updates

---
+++ precache-manifest.f9b78b0a4905a71a1118a04f240af207.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ad8a76e63506764cb0d23f44a71b8a4",
+    "url": "/Clicky-game/index.html"
+  },
+  {
+    "revision": "10da959c9bbd555704d0",
+    "url": "/Clicky-game/static/css/main.b2c404c4.chunk.css"
+  },
+  {
+    "revision": "bf939ec5a5a33e209433",
+    "url": "/Clicky-game/static/js/2.08f4d5dd.chunk.js"
+  },
+  {
+    "revision": "10da959c9bbd555704d0",
+    "url": "/Clicky-game/static/js/main.6d7a1f0d.chunk.js"
+  },
+  {
+    "revision": "cda0924bd23e3d9029fa",
+    "url": "/Clicky-game/static/js/runtime~main.952307d2.js"
+  },
+  {
+    "revision": "f819993cd2460a7beab18461547c33e9",
+    "url": "/Clicky-game/static/media/1.f819993c.jpg"
+  },
+  {
+    "revision": "50edc33920f0411f69d3c807be2caa97",
+    "url": "/Clicky-game/static/media/2.50edc339.jpg"
+  },
+  {
+    "revision": "8696741e775dd1e2e97c50b8446cdc02",
+    "url": "/Clicky-game/static/media/3.8696741e.jpeg"
+  },
+  {
+    "revision": "411aa6cabcf56d9e366eb2f0e73bc559",
+    "url": "/Clicky-game/static/media/4.411aa6ca.jpg"
+  },
+  {
+    "revision": "3ecc74fec549a2c98224c2444341a33c",
+    "url": "/Clicky-game/static/media/5.3ecc74fe.jpg"
+  },
+  {
+    "revision": "de3c3d2af621cb36a2a1aa0275d63170",
+    "url": "/Clicky-game/static/media/6.de3c3d2a.jpg"
+  },
+  {
+    "revision": "4a232a29bad5435af22a112cd5fad6f0",
+    "url": "/Clicky-game/static/media/7.4a232a29.jpg"
+  },
+  {
+    "revision": "9ff5148d163c012ca47536a2864c76c1",
+    "url": "/Clicky-game/static/media/8.9ff5148d.jpg"
+  },
+  {
+    "revision": "989eb88ade8d4a89acf112f9948ea45f",
+    "url": "/Clicky-game/static/media/9.989eb88a.jpg"
+  }
+]);
